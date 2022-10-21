bf11428615dd8071007b1720118436aade9da8a8
Jonathan Strickland <djanatyn@gmail.com>
2022-10-21 15:42:30 -0400

    start inspecting turnips

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,63 @@
+use peppi::model::enums::item::Type;
 use peppi::model::frame::Frame;
 use peppi::model::game::Frames;
 use peppi::model::item::Item;
+use std::collections::HashMap;
 use std::{env, fs, io};
 
-const BOBOMB_ID: u32 = 6;
-const MR_SATURN_ID: u32 = 7;
-const BEAM_SWORD_ID: u32 = 12;
-const TURNIP_ID: u32 = 99;
+/// Possible turnip faces.
+#[derive(Debug)]
+enum TurnipFace {
+    Normal,  // 0-4
+    Winky,   // 5
+    DotEyes, // 6
+    Stitch,  // 7
+}
 
-const PULLABLES: [u32; 4] = [BOBOMB_ID, MR_SATURN_ID, BEAM_SWORD_ID, TURNIP_ID];
+/// Index pulled turnip faces by item ID.
+type TurnipLog = HashMap<u32, TurnipFace>;
 
-/// Filter items for Peach's Vegetable pulls.
-fn peach_items(items: Vec<Item>) -> Vec<Item> {
-    items
-        .iter()
-        .filter(|item| PULLABLES.contains(&item.id))
-        .cloned()
-        .collect()
+/// Check to see if the item is a turnip.
+/// If it is, return the item ID + TurnipFace.
+fn is_turnip(item: &Item) -> Option<(u32, TurnipFace)> {
+    if item.r#type == Type::PEACH_TURNIP {
+        let face: TurnipFace = match item.state.0 {
+            0 => TurnipFace::Normal,
+            1 => TurnipFace::Normal,
+            2 => TurnipFace::Normal,
+            3 => TurnipFace::Normal,
+            4 => TurnipFace::Normal,
+            5 => TurnipFace::Winky,
+            6 => TurnipFace::DotEyes,
+            7 => TurnipFace::Stitch,
+            _ => panic!("unknown state"),
+        };
+
+        Some((item.id, face))
+    } else {
+        None
+    }
+}
+
+fn log_peach_items(log: &mut TurnipLog, items: Vec<Item>) {
+    for item in items {
+        if let Some((id, face)) = is_turnip(&item) {
+            (*log).insert(id, face);
+        }
+    }
 }
 
 /// Search frames for Peach's items.
 fn find_items(frames: Vec<Frame<2>>) {
+    let mut log: TurnipLog = HashMap::new();
+
     for frame in frames {
         if let Some(items) = frame.items {
-            let pulls = peach_items(items);
-            // show items peach pulled
-            if !pulls.is_empty() {
-                dbg!(pulls);
-            }
+            log_peach_items(&mut log, items);
         }
     }
+
+    dbg!(log);
 }
 
 fn main() {
